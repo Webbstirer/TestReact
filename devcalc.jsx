a424acf719e1d18ec2803604b59d07276da791e9
Webbstirer <daniel.webster@remarkableitsolutions.com>
2019-06-03 14:26:40 -0400

    Add files via upload

--- devcalc.jsx
+++ devcalc.jsx
@@ -1,54 +1,59 @@
-var DevChkBox = props =>(
-	<div>
-	<input id={props.id} type="checkbox" checked={props.checked} onChange={props.onChange} />
-		<span style={props.textstyle}>{props.text}</span>
-		</div>
-);
-
+const DevChkBox = props =>
+    // use the standard dev
+    <div>
+        <input id={props.id} type="checkbox" checked={props.checked} onChange={props.onChange} />
+        <span style={props.textstyle}>{props.text}</span>
+    </div>
 
 class Devotes extends React.Component {
-  constructor(props) {
-    super(props);
-		this.state = {checkedItems:{}};
-		this.onChange=this.onChange.bind(this);
-		this.getChecked=this.getChecked.bind(this);
-		this.changeChecked=this.changeChecked.bind(this);
-		//this.render=this.render.bind(this);
-	}
-	getChecked(id){
-		return this.state.checkedItems[id]===true;
-	}
-	changeChecked(id,value,oldState){
-		if(value !== true && value !== false) throw('bad value' + value);
-		var setting = {};
-		setting[id] = value;
-		var nextChecked=Object.assign({},oldState,setting);
-		return nextChecked;
-	}
-	onChange(e){
-		if(!e.id){
-			return;
-		}
-		var nextChecked = this.changeChecked(e.id,!this.getChecked(e.id),this.state.checkedItems);	
-		this.setState({checkedItems:nextChecked});
-	}
-	render(){
-		var self = () => this;
-		var values = [
-			{id:'crBlue',minText:"Crossroads Blue - +18 Defensive Ability",maxText:"Crossroads Blue - +24 Defensive Ability"}
-		]
-		
-		return (<div>Hello
-				{ values.map( v => (<DevChkBox 
-															key={v.id} 
-															id={v.id}
-															text={this.getChecked(v.id)?v.minText:v.maxText} 
-															onChange={this.onChange.bind(null,v)} 
-															textstyle={this.getChecked(v.id)?{fontStyle:'italic'}:{fontStyle:'normal'}}
-															/>))}
-				
-				</div>)
-	}
+    constructor(props){
+        super(props);
+        this.state={checkedItems:{},useMax:false};
+        // this.onChange=this.onChange.bind(this);
+        // this.getChecked=this.getChecked.bind(this);
+        // this.changeChecked=this.changeChecked.bind(this);
+        Object.getOwnPropertyNames(Devotes.prototype).filter(x => x != "constructor").map(x => {
+            if (typeof (this[x]) === "function")
+                this[x] = this[x].bind(this);
+        });
+    }
+    getChecked(id){
+        return this.state.checkedItems[id]===true;
+    }
+    changeChecked(id,value,oldState){
+        if(value !== true && value !== false) throw('bad value' + value);
+        var setting = {};
+        setting[id] = value;
+        var nextChecked=Object.assign({},oldState,setting);
+        return nextChecked;
+    }
+    onChange(e){
+        if(!e.id){
+            return;
+        }
+        var nextChecked = this.changeChecked(e.id,!this.getChecked(e.id),this.state.checkedItems);
+        this.setState({checkedItems:nextChecked,useMax:this.getChecked('special')});
+    }
+    render(){
+        var values = [
+            {id:'crBlue',minText:"Crossroads Blue - +18 Defensive Ability",maxText:"Crossroads Blue - +24 Defensive Ability"},
+            {id:'special',text:"Max Devotions"}
+        ]
+
+        return (<div>Hello
+                { values.map( v =>
+                    (<DevChkBox
+                        key={v.id}
+                        id={v.id}
+                        text={v.text? v.text : this.getChecked('special')?v.maxText:v.minText}
+                        onChange={this.onChange.bind(null,v)}
+                        textstyle={this.getChecked(v.id)?{fontStyle:'normal'}:{fontStyle:'italic'}}
+                        />)
+                    )
+                }
+
+                </div>)
+    }
 }
 
 ReactDOM.render(<Devotes />, document.getElementById("app"));
