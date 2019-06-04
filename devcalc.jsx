5a4c29a051e9d94d2f33de45fed67bf83625a989
Webbstirer <daniel.webster@remarkableitsolutions.com>
2019-06-03 20:29:59 -0400

    Add files via upload

--- devcalc.jsx
+++ devcalc.jsx
@@ -49,18 +49,25 @@ class Devotes extends React.Component {
             {id:'anv2',minText:"+10 Physique",maxText:"+10 Physique"},
             {id:'anv3',minText:"Increases Armor Absorption by 3%",maxText:"Increases Armor Absorption by 3%"},
             {id:'anv4',minText:"+15 Defensive Ability, +20% Constitution",maxText:"+15 Defensive Ability, +20% Constitution"},
-            {id:'anv5',minText:"Targo's Hammer - 0.1 Second skill Recharge, 100% Chance to pass through Enemies, 0.3 Meter Radius, 15% Weapon Damage, 28 Physical Damage, +50% Internal Trauma Damage, 10% Chance of Stun target for 1 Second"}
+            {id:'anv5',minText:"Targo's Hammer - 0.1 Second skill Recharge, 100% Chance to pass through Enemies, 0.3 Meter Radius, 15% Weapon Damage, 28 Physical Damage, +50% Internal Trauma Damage, 10% Chance of Stun target for 1 Second",maxText:"Targo's Hammer(20/20) - 0.1 Second skill Recharge, 100% Chance to pass through Enemies, 0.3 Meter Radius, 35% Weapon Damage, 168 Physical Damage, +250% Internal Trauma Damage, 50% Chance of Stun target for 1 Second"}
         ]
 
         var values3 = [
-            {id:'anv1',minText:"+15 Defensive Ability",maxText:"+15 Defensive Ability"},
-            {id:'anv2',minText:"+10 Physique",maxText:"+10 Physique"},
-            {id:'anv3',minText:"Increases Armor Absorption by 3%",maxText:"Increases Armor Absorption by 3%"},
-            {id:'anv4',minText:"+15 Defensive Ability, +20% Constitution",maxText:"+15 Defensive Ability, +20% Constitution"},
-            {id:'anv5',minText:"Targo's Hammer - 0.1 Second skill Recharge, 100% Chance to pass through Enemies, 0.3 Meter Radius, 15% Weapon Damage, 28 Physical Damage, +50% Internal Trauma Damage, 10% Chance of Stun target for 1 Second"}
+            {id:'et1',minText:"15% reduced Stun Duration",maxText:"15% reduced Stun Duration"},
+            {id:'et2',minText:"8% Pierce Resistance, Bonus to All Pets: 8% Pierce Resistance",maxText:"8% Pierce Resistance, Bonus to All Pets: 8% Pierce Resistance"},
+            {id:'et3',minText:"8% Chaos Resistance, Bonus to All Pets: 8% Chaos Resistance",maxText:"8% Chaos Resistance, Bonus to All Pets: 8% Chaos Resistance"},
+            {id:'et4',minText:"8% Aether Resistance, Bonus to All Pets: 8% Aether Resistance",maxText:"8% Aether Resistance, Bonus to All Pets: 8% Aether Resistance"},
         ]
 
-        return (<div><H1>Grim Dawn Calculator</H1>
+        var values4 = [
+            {id:'falc1',minText:"+15% Physical Damage",maxText:"+15% Physical Damage"},
+            {id:'falc2',minText:"+24% Bleeding Damage",maxText:"+24% Bleeding Damage"},
+            {id:'falc3',minText:"+15 Cunning",maxText:"+15 Cunning"},
+            {id:'falc4',minText:"+24% Physical Damage",maxText:"+24% Physical Damage"},
+            {id:'falc5',minText:"Falcon Swoop - 2 Second Skill Recharge, 3 Projectile(s), 100% Chance to pass through Enemies, 0.1 Meter Radius, 6% Weapon Damage, 28 Physical Damage 150 Bleeding Damage over 3 Seconds",maxText:"Falcon Swoop(20/20) - 2 Second Skill Recharge, 6 Projectile(s), 100% Chance to pass through Enemies, 0.1 Meter Radius, 20% Weapon Damage, 98 Physical Damage 405 Bleeding Damage over 3 Seconds"}
+        ]
+
+        return (<div><b>Crossroads</b>
                 { values.map( v =>
                     (<DevChkBox
                         key={v.id}
@@ -71,7 +78,7 @@ class Devotes extends React.Component {
                         />)
                     )
                 }
-                <H2>Anvil</H2>
+                <b>Anvil</b>
                 { values2.map( v2 =>
                     (<DevChkBox
                         key={v2.id}
@@ -82,7 +89,7 @@ class Devotes extends React.Component {
                         />)
                     )
                 }
-                <H2>Empty Throne</H2>
+                <b>Empty Throne</b>
                 { values3.map( v3 =>
                     (<DevChkBox
                         key={v3.id}
@@ -90,6 +97,17 @@ class Devotes extends React.Component {
                         text={v3.text? v3.text : this.getChecked('special')?v3.maxText:v3.minText}
                         onChange={this.onChange.bind(null,v3)}
                         textstyle={this.getChecked(v3.id)?{fontStyle:'normal'}:{fontStyle:'italic'}}
+                        />)
+                    )
+                }
+                <b>Falcon</b>
+                { values4.map( v4 =>
+                    (<DevChkBox
+                        key={v4.id}
+                        id={v4.id}
+                        text={v4.text? v4.text : this.getChecked('special')?v4.maxText:v4.minText}
+                        onChange={this.onChange.bind(null,v4)}
+                        textstyle={this.getChecked(v4.id)?{fontStyle:'normal'}:{fontStyle:'italic'}}
                         />)
                     )
                 }
